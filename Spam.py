2a95302ca88802b539ef1417cb57084f9f121aa7
Omkar Jadhav <106991458+FatGuyy@users.noreply.github.com>
2022-07-12 11:19:34 +0530

    added stop at 100

--- Spam.py
+++ Spam.py
@@ -1,9 +1,10 @@
 import pyautogui 
 import time
-
+n=0
 time.sleep(5)
 f = open('text1.txt', 'r')
-while True:
+while n < 100:
+    n += 1
     for word in f:
         time.sleep(0.3)
         pyautogui.typewrite(word)
